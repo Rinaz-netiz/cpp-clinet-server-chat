8271ae3e09ded394c77b006c004ce4337d6f0aa4
rinaz <garaevribaz@gmail.com>
2025-01-24 10:09:03 +0300

    smth work

--- server.cpp
+++ server.cpp
@@ -1,10 +1,31 @@
-//
-// Created by Rinaz on 24.01.2025.
-//
-
 /*
 Написать клиент-серверное приложение
 Реализовать что-то типо мессенджера, сообщение должно шифровать на стороне клиента
 Клиент должен работать через терминал, шифрование должно быть симметричным
 */
+#ifndef WIN32_LEAN_AND_MEAN
+#define WIN32_LEAN_AND_MEAN
+#endif
 
+#include <windows.h>
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#include <iphlpapi.h>
+#include <stdio.h>
+
+#pragma comment(lib, "Ws2_32.lib")
+
+int main() {
+
+    WSADATA wsaData;
+
+    int iResult;
+
+    iResult = WSAStartup(MAKEWORD(2,2), &wsaData);
+    if (iResult != 0) {
+        printf("WSAStartup failed: %d\n", iResult);
+        return 1;
+    }
+
+    return 0;
+}
