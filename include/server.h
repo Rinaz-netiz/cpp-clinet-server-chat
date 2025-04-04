86321bdcedae307f39ed1b95db93959bf1f01b17
alister <artemyudin.2006@yandex.ru>
2025-04-04 23:17:52 +0300

    init
    # Conflicts:
    #	CMakeLists.txt
    #	include/server.h
    #	src/server.cpp

--- include/server.h
+++ include/server.h
@@ -4,26 +4,5 @@
 
 #ifndef SERVER_H
 #define SERVER_H
-#include <sys/epoll.h>
-
-class Server {
-    int listen_sock;
-    int efd ;
-    epoll_event ev{};
-
-    void create_sock();
-    void setup_epoll();
-    void listen_con();
-    void receive_msg();
-    void send_msg();
-    void msg_handler();
-
-public:
-    Server();
-    ~Server();
-
-    void run();
-};
-
 
 #endif //SERVER_H
