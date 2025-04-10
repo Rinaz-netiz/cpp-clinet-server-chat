db48bfc2491653f8b42d3c2688d42396106b38b0
alister <artemyudin.2006@yandex.ru>
2025-04-10 20:19:59 +0300

    socket connection

--- include/server.h
+++ include/server.h
@@ -4,5 +4,26 @@
 
 #ifndef SERVER_H
 #define SERVER_H
+#include <sys/epoll.h>
+
+class Server {
+    int listen_sock;
+    int efd ;
+    epoll_event ev{};
+
+    void create_sock();
+    void setup_epoll();
+    void listen_con();
+    void receive_msg();
+    void send_msg();
+    void msg_handler();
+
+public:
+    Server();
+    ~Server();
+
+    void run();
+};
+
 
 #endif //SERVER_H
