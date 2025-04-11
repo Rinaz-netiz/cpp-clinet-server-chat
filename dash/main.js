6ed75d394fba641b79e86c6b04d86afa307e3d8c
Rinaz <garaevribaz@gmail.com>
2025-04-11 17:44:12 +0300

    updating design, python, js code

--- dash/main.js
+++ dash/main.js
@@ -1,18 +1,28 @@
 window.addEventListener("DOMContentLoaded", () => {
     // Open the WebSocket connection and register event handlers.
+    let id = "id" + Math.random().toString(16).slice(2)
+
     const websocket = new WebSocket("ws://localhost:8769/");
 
+    websocket.addEventListener("error", (event) => {
+        console.log("WebSocket error: ", event);
+    });
+
     receiveMessage(websocket);
-    sendMessage(websocket);
+    sendMessage(websocket, id);
 
 });
 
-function sendMessage(websocket) {
-    const form = document.querySelector("#userinfo");
+function sendMessage(websocket, id) {
+    const form = document.querySelector("#sendMessage");
 
-    form.addEventListener("submit", async (event) => {
-        const msg = document.querySelector("#username").value;
-        const data = {id: 1, msg: msg};
+    form.addEventListener("click", async (event) => {
+        const msg = document.querySelector("#messageText").value;
+        if(!msg)
+            return
+        const data = {name: id, msg: msg};
+
+        addNewMessage("chat-message-right mb-4", data)
 
         websocket.send(JSON.stringify(data));
     });
@@ -20,13 +30,32 @@ function sendMessage(websocket) {
 
 function receiveMessage(websocket) {
     websocket.addEventListener("message", ({ data }) => {
-        console.log(data)
-        // const event = data
-        //
-        // console.log(event)
+        const res = JSON.parse(data);
+        addNewMessage("chat-message-left pb-4", res);
     });
 }
 
 
+function addNewMessage(classesType, data) {
+    const subject = document.querySelector("#chat");
 
+    let div = document.createElement("div");
+    div.setAttribute('class', classesType);
+
+    div.innerHTML = `
+            <div>
+            <img src="https://bootdey.com/img/Content/avatar/avatar1.png" class="rounded-circle mr-1" alt="Chris Wood" width="40" height="40">
+            <div class="text-muted small text-nowrap mt-2">2:43 am</div>
+            </div>
+            <div class="flex-shrink-1 bg-light rounded py-2 px-3 mr-3">
+            <div class="font-weight-bold mb-1">${data["name"]}</div>
+            ${data["msg"]}
+            </div>
+        `
+
+    subject.insertAdjacentElement(
+        "beforeend",
+        div,
+    );
+}
 
