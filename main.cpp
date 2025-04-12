6da8885d6f3304655bfc225b530d0cf0eb6f2926
Rinaz <garaevribaz@gmail.com>
2025-04-12 16:22:18 +0300

    adding autorization and usernames in the cookies

--- main.cpp
+++ main.cpp
@@ -1,13 +1,8 @@
-//
-// Created by alister_mint on 06.04.25.
-//
 #include "include/server.h"
-#include <iostream>
 
 
 int main() {
     Server server;
 
-    std::cout << "Server start" << std::endl;
     server.run();
 }
